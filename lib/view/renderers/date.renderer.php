0cee9755ca2c6499d39830fc25d11e7cc6b09298
martind <martin@dekstop.de>
2010-01-24 18:15:59 +0000

    added string parser to date_renderer (using strtotime)

--- lib/view/renderers/date.renderer.php
+++ lib/view/renderers/date.renderer.php
@@ -2,8 +2,20 @@
 
 function date_renderer($property, $args) {
   if ($property->is_null()) return null;
-  $timestamp = $property->raw();
-  if (!is_numeric($timestamp)) return null;
+
+  $timestamp = null;
+  if (is_numeric($property->raw())) {
+    // it is a number or numeric string, we handle it as timestamp
+    $timestamp = (int)$property->raw();  
+  } else {
+    // strtotime should handle it
+    $timestamp = strtotime($property->raw());
+    if ($timestamp == -1 || $timestamp === false) {
+      // strtotime() was not able to parse $string
+        return null;
+    }
+  }
+  
   if (count($args)!=1) throw new Exception('Function requires one parameter. Provided: ' . count($args));
   return date($args[0], $timestamp);
 }
--- lib/view/renderers/date.renderer.test.php
+++ lib/view/renderers/date.renderer.test.php
@@ -18,6 +18,11 @@ class DateRendererTest extends RendererUnitTestCase {
     $this->assertEqual('2009-02-13 23:31', $this->render(1234567890, 'date', 'Y-m-d H:i'));
   }
 
+  function test_string_timestamps() {
+    $this->assertEqual('2009-02-13 23:31', $this->render('2009-02-13 23:31', 'date', 'Y-m-d H:i'));
+    $this->assertEqual('2009-07-09 03:27', $this->render('2009-07-09 03:27:48.064122', 'date', 'Y-m-d H:i'));
+  }
+
   function test_no_args() {
     $this->expectException();
     $this->render(1234567890, 'date');
